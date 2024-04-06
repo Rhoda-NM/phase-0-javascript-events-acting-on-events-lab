13d3584f93ab212ef12b3a36e96132358bd0ecf3
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-04-06 14:57:27 +0300

    moving dodger

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.background = "#ff69b4";
+dodger.style.bottom = "0px";
+dodger.style.left = "180px";
+dodger.style.right = "180px";
+document.addEventListener('keydown', function(event) {
+    //console.log(event);
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();  
+    }
+    else if( event.key === "ArrowRight") {
+       moveDodgerRight();
+    }
+    
+})
+function moveDodgerLeft () {
+    const leftNum = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNum, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+        console.log(left);
+    }
+}
+function moveDodgerRight() {
+    const leftNum = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNum, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
